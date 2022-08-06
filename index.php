675595eead670bc2d36d2b0110b3882a425ebf36
RodrigoTallar <rtallarr@gmail.com>
2022-08-06 16:56:31 -0400

    eliminar codigo anterior

--- index.php
+++ index.php
@@ -94,42 +94,7 @@
         <footer class="copyright">
             &copy; 2021 Rodrigo Tallar.
         </footer>
-
-    <?php
-        if(isset($_POST['submit'])) {
-
-        function CheckCaptcha($userResponse) {
-            $fields_string = '';
-            $fields = array(
-                'secret' => '6LeOzGogAAAAABHvAcQ2jdemEpdIyjAwmQRGf8sb',
-                'response' => $userResponse
-            );
-            foreach($fields as $key=>$value)
-            $fields_string .= $key . '=' . $value . '&';
-            $fields_string = rtrim($fields_string, '&');
-
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
-            curl_setopt($ch, CURLOPT_POST, count($fields));
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
-
-            $res = curl_exec($ch);
-            curl_close($ch);
-
-            return json_decode($res, true);
-        }
-
-        $result = CheckCaptcha($_POST['g-recaptcha-response']);
-
-        if ($result['success']) {
-            echo "Captcha verified Successfully";
-        } else {
-            echo '<script>alert("Error Message");</script>';
-            }
-        }
-    ?>
-
+        
     <script src="https://www.google.com/recaptcha/api.js" async defer></script> <!-- recaptcha -->
     <script src="https://s.pageclip.co/v1/pageclip.js" charset="utf-8"></script> <!-- Pageclip -->
 
